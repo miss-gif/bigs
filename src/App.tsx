608ac212156189a2a18cc092ec121ffb447258ed
miss-gif <svx327@naver.com>
2024-11-16 15:01:10 +0900

    Route: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Login from "./features/auth/pages/Login";
+import SignUp from "./features/auth/pages/SignUp";
+import BoardDetail from "./features/board/pages/BoardDetail";
+import BoardEdit from "./features/board/pages/BoardEdit";
+import BoardList from "./features/board/pages/BoardList";
+import BoardWrite from "./features/board/pages/BoardWrite";
+import NotFoundPage from "./pages/NotFoundPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* 인증 */}
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        {/* 게시판 */}
+        <Route path="boards" element={<BoardList />} />
+        <Route path="boards/:id" element={<BoardDetail />} />
+        <Route path="boards/write" element={<BoardWrite />} />
+        <Route path="boards/edit/:id" element={<BoardEdit />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/features/board/pages/BoardEdit.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BoardEdit = () => {
+  return <div>BoardEdit</div>;
+};
+
+export default BoardEdit;
